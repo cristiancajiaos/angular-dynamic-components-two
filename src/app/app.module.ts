07082a42697227dba693ae78f8b1bd3f9869e3d7
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-02-16 16:12:20 -0300

    Creado componente dinámico y método para añadirlo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    DynamicComponent,
   ],
   imports: [
     BrowserModule
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { DynamicComponent } from '../dynamic/dynamic.component';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  add() {
+    const dynamicComponentFactory = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
+    const componentRef = this.container.createComponent(dynamicComponentFactory);
   }
 
 }
